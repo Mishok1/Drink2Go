d8e0d6b6cabf792166c70f26d28d358a0019201b
Mishok1 <mm21112000@gmail.com>
2024-06-20 13:41:54 +0300

    Adds header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+
+const menuButton = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('menu--mobile-open');
+  menuButton.classList.toggle('navigation__button--menu-open');
+});
