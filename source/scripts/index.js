83e586ee8848980264085cf92cdc332a8ed01d0e
Mishok1 <mm21112000@gmail.com>
2024-09-24 19:57:20 +0300

    Adds header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+
+const menu = document.querySelector('#menu');
+const menuButton = document.querySelector('#menu-button');
+
+menuButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  menuButton.classList.toggle('navigation__button--open');
+  menu.classList.toggle('menu--open');
+});
