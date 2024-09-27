2085aaae7e8fabfece17e03da06460f7ecfcd368
Mishok1 <mm21112000@gmail.com>
2024-09-27 14:46:32 +0300

    Adds catalog

---
+++ source/scripts/range-slider.js
@@ -0,0 +1,39 @@
+import noUiSlider from 'nouislider';
+
+const rangeSlider = document.querySelector('.range-slider__range');
+const inputMinEl = document.querySelector('.range-slider__price--from');
+const inputMaxEl = document.querySelector('.range-slider__price--to');
+const inputsEl = [inputMinEl, inputMaxEl];
+
+
+const initRangeSlider = () => {
+  if (rangeSlider) {
+    noUiSlider.create(rangeSlider, {
+      start: [0, 900],
+      step: 1,
+      connect: true,
+      range: {
+        'min': [0],
+        'max': [980]
+      }
+    });
+
+    rangeSlider.noUiSlider.on('update', (currentValues, handleIndex) => {
+      inputsEl[handleIndex].value = Math.round(currentValues[handleIndex]) || '';
+    });
+
+    const setRangeSlider = (input, value) => {
+      const rangeValues = [null, null];
+      rangeValues[input] = value;
+      rangeSlider.noUiSlider.set(rangeValues);
+    };
+
+    inputsEl.forEach((input, index) => {
+      input.addEventListener('change', (event) => {
+        setRangeSlider(index, event.currentTarget.value);
+      });
+    });
+  }
+};
+
+initRangeSlider();
